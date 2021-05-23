48f1efb62a6b41c811c25afca0e4595ac444f765
jaechung77 <jaechung77@gmail.com>
2021-05-23 21:46:18 +1000

    loop and iteration

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards = (arr, adj) => {
+    const msg = []
+    for (let i=0; i < arr.length; i++){
+        msg[i] = `Thank you, ${arr[i]}, for the wonderful ${adj} gift!`
+    }
+    return msg
+}
+
+const countDown = (num) =>{
+    while(num >=0){
+        console.log(num)
+        num--
+    }
+}
+
+//countDown(10)
